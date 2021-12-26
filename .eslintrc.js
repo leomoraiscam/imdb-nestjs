961bf28be0a3bc780a89d8e8841d5c93941b34f7
lmorais <leozonnn@gmail.com>
2021-12-26 16:06:17 -0300

    add new rules in eslint and create eslint ignore file

--- .eslintrc.js
+++ .eslintrc.js
@@ -20,5 +20,25 @@ module.exports = {
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
     '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-unused-vars': [
+      'error',
+      {
+        argsIgnorePattern: '_',
+      },
+    ],
+    'import-helpers/order-imports': [
+      'warn',
+      {
+        newlinesBetween: 'always',
+        groups: [
+          ['module', '/^@nestjs/'],
+          '/^@config/',
+          '/^@shared/',
+          '/^@modules/',
+          ['parent', 'sibling', 'index'],
+        ],
+        alphabetize: { order: 'asc', ignoreCase: true },
+      },
+    ],
   },
 };
