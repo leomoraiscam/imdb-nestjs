59edabfe4418e11e10bbda2c43dda76b4e3cf5af
lmorais <leozonnn@gmail.com>
2022-01-28 00:24:16 -0300

    refactor: change auth provider in straregies

--- src/modules/users/providers/AuthProvider/authProvider.module.ts
+++ src/modules/users/providers/AuthProvider/authProvider.module.ts
@@ -3,8 +3,8 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { UsersRepository } from '../../infra/typeorm/repositories/UsersRepository';
 import { HashProviderModule } from '../HashProvider/hashProvider.module';
-import { JwtStrategy } from './implementations/JwtStrategyService.service';
-import { LocalStrategy } from './implementations/LocalStrategyService.service';
+import { JwtStrategy } from './implementations/jwt.strategy';
+import { LocalStrategy } from './implementations/local.strategy';
 
 @Module({
   imports: [TypeOrmModule.forFeature([UsersRepository]), HashProviderModule],
---
+++ src/modules/users/providers/AuthProvider/implementations/jwt.strategy.ts
@@ -0,0 +1,27 @@
+import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt, Strategy } from 'passport-jwt';
+
+import { jwt } from '../../../../../config/auth';
+import { UsersRepository } from '../../../infra/typeorm/repositories/UsersRepository';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor(private usersRepository: UsersRepository) {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: jwt.secret,
+    });
+  }
+
+  async validate(payload: any) {
+    const user = await this.usersRepository.findById(payload.sub);
+
+    if (!user) {
+      throw new UnauthorizedException('invalid-token');
+    }
+
+    return user;
+  }
+}
