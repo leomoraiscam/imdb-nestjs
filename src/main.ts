bf5aa4f24159a7ce24e5cc8540b8a90276e4a140
lmorais <leozonnn@gmail.com>
2022-02-03 15:59:25 -0300

    feat: configuration swagger in main file

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('IMDB')
+    .setVersion('v1')
+    .build();
+
+  const swaggerDocument = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('api/docs', app, swaggerDocument);
+
   await app.listen(3000);
 }
 bootstrap();
