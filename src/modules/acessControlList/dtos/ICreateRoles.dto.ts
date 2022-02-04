9db027f840ad34aded26cab2901b7adbc62e883b
lmorais <leozonnn@gmail.com>
2022-02-04 08:57:21 -0300

    feat: use decorators to validate data in create role and definition values to enuns

--- src/modules/acessControlList/dtos/ICreateRoles.dto.ts
+++ src/modules/acessControlList/dtos/ICreateRoles.dto.ts
@@ -1,4 +1,16 @@
-export interface ICreateRolesDTO {
-  name: string;
+import { ApiProperty } from '@nestjs/swagger';
+import { IsIn, IsNotEmpty, IsString } from 'class-validator';
+
+type EnumTypes = 'Role' | 'User';
+
+export class CreateRolesDTO {
+  @ApiProperty()
+  @IsIn(['ADMIN', 'USER'])
+  @IsNotEmpty()
+  name: EnumTypes;
+
+  @ApiProperty()
+  @IsString()
+  @IsNotEmpty()
   description: string;
 }
