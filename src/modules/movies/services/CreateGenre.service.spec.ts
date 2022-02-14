b44ab205ccf4a2977d93383a8e37332db9735961
lmorais <leozonnn@gmail.com>
2022-02-14 14:46:26 -0300

    feat: implementation create genre service

---
+++ src/modules/movies/services/CreateGenre.service.spec.ts
@@ -0,0 +1,37 @@
+import { ConflictException } from '@nestjs/common';
+
+import { InMemoryGenresRepository } from '../repositories/in-memory/InMemoryGenres.repository';
+import { CreateGenreService } from './CreateGenre.service';
+
+describe('Create Genres', () => {
+  let createGenreService: CreateGenreService;
+  let inMemoryGenresRepository: InMemoryGenresRepository;
+
+  beforeEach(async () => {
+    inMemoryGenresRepository = new InMemoryGenresRepository();
+    createGenreService = new CreateGenreService(inMemoryGenresRepository);
+  });
+
+  it('should be able to create a genre', async () => {
+    const role = await createGenreService.execute({
+      name: 'action',
+      description: 'action genre',
+    });
+
+    expect(role).toHaveProperty('id');
+  });
+
+  it('should not be able to create a role when the same exist', async () => {
+    const genre = await inMemoryGenresRepository.create({
+      name: 'adventure',
+      description: 'adventure genre',
+    });
+
+    await expect(
+      createGenreService.execute({
+        name: genre.name,
+        description: 'adventure genre',
+      }),
+    ).rejects.toBeInstanceOf(ConflictException);
+  });
+});
---
+++ src/modules/movies/services/CreateGenre.service.ts
@@ -0,0 +1,27 @@
+import { ConflictException, Inject } from '@nestjs/common';
+
+import { ICreateGenreDTO } from '../dtos/ICreateGenre.dto';
+import { Genre } from '../infra/typeorm/entities/Genre.entity';
+import { IGenresRepository } from '../repositories/IGenresRepository.interface';
+
+export class CreateGenreService {
+  constructor(
+    @Inject('GENRE_REPOSITORY')
+    private genresRepository: IGenresRepository,
+  ) {}
+
+  async execute({ name, description }: ICreateGenreDTO): Promise<Genre> {
+    const genreExist = await this.genresRepository.findByName(name);
+
+    if (genreExist) {
+      throw new ConflictException('genre already exist');
+    }
+
+    const genre = await this.genresRepository.create({
+      name,
+      description,
+    });
+
+    return genre;
+  }
+}
