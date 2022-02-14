c173c074441558838cd4fbccfb8059f8d7dce82c
lmorais <leozonnn@gmail.com>
2022-02-14 14:44:22 -0300

    feat: implementation genre entity

---
+++ src/modules/movies/infra/typeorm/entities/Genre.entity.ts
@@ -0,0 +1,31 @@
+import { ApiProperty } from '@nestjs/swagger';
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+
+@Entity('genres')
+export class Genre {
+  @ApiProperty()
+  @PrimaryGeneratedColumn('uuid')
+  id: string;
+
+  @ApiProperty()
+  @Column()
+  name: string;
+
+  @ApiProperty()
+  @Column()
+  description: string;
+
+  @ApiProperty()
+  @CreateDateColumn()
+  created_at: Date;
+
+  @ApiProperty()
+  @UpdateDateColumn()
+  updated_at: Date;
+}
