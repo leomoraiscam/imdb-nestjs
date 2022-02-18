57c6b08cc5a661586434faa872c1a2aecd6a9e90
lmorais <leozonnn@gmail.com>
2022-02-18 09:41:54 -0300

    feat: implementation interface to infere type in list movies

---
+++ src/modules/movies/dtos/ISerializedMovies.dto.ts
@@ -0,0 +1,26 @@
+export interface ISerializedResponse {
+  id: string;
+  name: string;
+  description: string;
+  author: string;
+  year: number;
+  duration: string;
+  created_at: Date;
+  updated_at: Date;
+  genres: Array<{
+    id: string;
+    name: string;
+    description: string;
+    created_at: Date;
+    updated_at: Date;
+  }>;
+  votes: Array<{
+    id: string;
+    note: number;
+    movie_id: string;
+    user_id: string;
+    created_at: Date;
+    updated_at: Date;
+  }>;
+  average: number;
+}
