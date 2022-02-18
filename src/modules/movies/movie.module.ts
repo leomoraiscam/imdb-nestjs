0e5757656b7a31826296ad0fcf0323bd8ddf9de5
lmorais <leozonnn@gmail.com>
2022-02-17 23:18:42 -0300

    chore: add new classes in movie module

--- src/modules/movies/movie.module.ts
+++ src/modules/movies/movie.module.ts
@@ -4,6 +4,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { GenresController } from './infra/http/controllers/Genre.controller';
 import { ListMoviesController } from './infra/http/controllers/ListMovies.controller';
 import { MoviesController } from './infra/http/controllers/Movie.controller';
+import { ShowMoviesController } from './infra/http/controllers/ShowMovies.controller';
 import { Genre } from './infra/typeorm/entities/Genre.entity';
 import { Movie } from './infra/typeorm/entities/Movie.entity';
 import { GenresRepository } from './infra/typeorm/repositories/Genres.repository';
@@ -11,10 +12,16 @@ import { MoviesRepository } from './infra/typeorm/repositories/Movies.repository
 import { CreateGenreService } from './services/CreateGenre.service';
 import { CreateMovieService } from './services/CreateMovie.service';
 import { ListMoviesServices } from './services/ListMovies.service';
+import { ShowMoviesServices } from './services/ShowMovies.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([Movie, Genre])],
-  controllers: [GenresController, MoviesController, ListMoviesController],
+  controllers: [
+    GenresController,
+    MoviesController,
+    ListMoviesController,
+    ShowMoviesController,
+  ],
   providers: [
     {
       provide: 'GENRE_REPOSITORY',
@@ -27,6 +34,7 @@ import { ListMoviesServices } from './services/ListMovies.service';
     CreateGenreService,
     CreateMovieService,
     ListMoviesServices,
+    ShowMoviesServices,
   ],
 })
 export class MovieModule {}
