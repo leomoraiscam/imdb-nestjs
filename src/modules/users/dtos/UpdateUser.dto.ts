93980afd07bc0a75e61b13cebeecab145e6ab02b
lmorais <lmorais.contato@gmail.com>
2022-12-06 11:17:23 -0300

    docs: add example in user update data transfer object

--- src/modules/users/dtos/UpdateUser.dto.ts
+++ src/modules/users/dtos/UpdateUser.dto.ts
@@ -1,13 +1,17 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { ValidateIf, IsNotEmpty, IsString } from 'class-validator';
+import { ApiProperty, PartialType } from '@nestjs/swagger';
+import { ValidateIf, IsNotEmpty, IsString, IsOptional } from 'class-validator';
 
 import { CreateUserDTO } from './CreateUser.dto';
 
 export class UpdateUserDTO extends PartialType(CreateUserDTO) {
   @IsString()
   @IsNotEmpty()
-  userId: string;
+  @IsOptional()
+  userId?: string;
 
+  @ApiProperty({
+    example: 'test@123',
+  })
   @ValidateIf((requestBody) => !!requestBody.password)
   @IsNotEmpty()
   oldPassword: string;
