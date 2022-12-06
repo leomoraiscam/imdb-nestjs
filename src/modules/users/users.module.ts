5c4c1231293598cc9df3309d34bfc7625b87b6ea
lmorais <lmorais.contato@gmail.com>
2022-12-06 11:18:01 -0300

    feat: add new controllers in user module

--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -5,11 +5,13 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { jwt } from '../../config/auth';
 import { AuthenticatedUserController } from './infra/http/controllers/AuthenticatedUser.controller';
 import { CreateUsersController } from './infra/http/controllers/CreateUser.controller';
-import { ProfileUserController } from './infra/http/controllers/ProfileUser.controller';
+import { ShowProfileUserController } from './infra/http/controllers/ShowProfileUser.controller';
+import { UpdateProfileUserController } from './infra/http/controllers/UpdateProfileUser.controller';
 import { User } from './infra/typeorm/entities/User.entity';
 import { UsersRepository } from './infra/typeorm/repositories/Users.repository';
 import { AuthProviderModule } from './providers/AuthProvider/authProvider.module';
 import { HashProviderModule } from './providers/HashProvider/hashProvider.module';
+import { BCryptHashProvider } from './providers/HashProvider/implementations/BCryptHash.provider';
 import { AuthenticateUserService } from './services/AuthenticateUser.service';
 import { CreateUserService } from './services/CreateUser.service';
 import { ShowProfileUserService } from './services/ShowProfileUser.service';
@@ -27,13 +29,18 @@ import { UpdateUserService } from './services/UpdateUser.service';
   ],
   controllers: [
     CreateUsersController,
-    ProfileUserController,
+    ShowProfileUserController,
     AuthenticatedUserController,
+    UpdateProfileUserController,
   ],
   providers: [
     {
       provide: 'USER_REPOSITORY',
       useClass: UsersRepository,
+    },
+    {
+      provide: 'HASH_PROVIDER',
+      useClass: BCryptHashProvider,
     },
     CreateUserService,
     ShowProfileUserService,
