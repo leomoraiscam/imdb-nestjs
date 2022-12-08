b79e223d183a59c8cd8ac508626f5db9ac0e5e1c
lmorais <lmorais.contato@gmail.com>
2022-12-07 22:57:56 -0300

    docs: add docs to role create

--- src/modules/acessControlList/dtos/CreateRoles.dto.ts
+++ src/modules/acessControlList/dtos/CreateRoles.dto.ts
@@ -4,12 +4,16 @@ import { IsIn, IsNotEmpty, IsString } from 'class-validator';
 type EnumTypes = 'admin' | 'user';
 
 export class CreateRolesDTO {
-  @ApiProperty()
+  @ApiProperty({
+    example: 'admin',
+  })
   @IsIn(['admin', 'user'])
   @IsNotEmpty()
   name: EnumTypes;
 
-  @ApiProperty()
+  @ApiProperty({
+    example: 'this role is the administrator',
+  })
   @IsString()
   @IsNotEmpty()
   description: string;
