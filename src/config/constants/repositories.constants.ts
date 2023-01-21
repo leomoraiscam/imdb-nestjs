366739612683b3ed8a14161fae7b76a0354e8daf
lmorais <lmorais.contato@gmail.com>
2023-01-21 12:07:16 -0300

    feat: create votes constants to repositories and tags

--- src/config/constants/repositories.constants.ts
+++ src/config/constants/repositories.constants.ts
@@ -2,3 +2,4 @@ export const MOVIES_REPOSITORY = 'MoviesRepository';
 export const GENRES_REPOSITORY = 'GenresRepository';
 export const DIRECTORS_REPOSITORY = 'DirectorsRepository';
 export const ACTORS_REPOSITORY = 'ActorsRepository';
+export const VOTES_REPOSITORY = 'VotesRepository';
--- src/config/constants/resourceTags.constants.ts
+++ src/config/constants/resourceTags.constants.ts
@@ -1,2 +1,3 @@
 export const MOVIES = 'Movies';
 export const GENRES = 'Genres';
+export const VOTES = 'Votes';
