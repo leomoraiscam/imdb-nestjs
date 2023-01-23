736fdf165a26ed602c68c6de891934a7ae967a02
lmorais <lmorais.contato@gmail.com>
2023-01-22 23:16:07 -0300

    chore: imports correct files in movie module

--- src/modules/movies/infra/http/controllers/CreateVote.controller.ts
+++ src/modules/movies/infra/http/controllers/CreateVote.controller.ts
@@ -7,6 +7,7 @@ import {
   UNAUTHORIZED_RESPONSE,
 } from '@/config/constants/responses.constant';
 import { RolesEnum } from '@/modules/accessControlList/dtos/roles.enum';
+import { CreateVotesToMovieService } from '@/modules/movies/services/CreateVotesToMovie.service';
 import {
   Body,
   Controller,
@@ -32,14 +33,13 @@ import { RolesGuard } from 'src/shared/guards/Roles.guard';
 import { JwtAuthGuard } from 'src/shared/infra/http/guards/jwtAuth.guard';
 
 import { CreateVotesDTO } from '../../../dtos/requests/CreateVotes.dto';
-import { CreateVotesToMoviesService } from '../../../services/CreateVotesToMovies.service';
 import { Vote } from '../../typeorm/entities/Vote.entity';
 
 @ApiTags(VOTES)
 @Controller('movies/:movieId/votes')
 export class CreateVoteController {
   constructor(
-    private readonly createVotesToMoviesService: CreateVotesToMoviesService,
+    private readonly createVotesToMovieService: CreateVotesToMovieService,
   ) {}
 
   @Post()
@@ -71,7 +71,7 @@ export class CreateVoteController {
     @Param('movieId') movieId: string,
     @Body() { note }: CreateVotesDTO,
   ): Promise<Vote> {
-    return this.createVotesToMoviesService.execute({
+    return this.createVotesToMovieService.execute({
       movieId,
       note,
       userId: id,
