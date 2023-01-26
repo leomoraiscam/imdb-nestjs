f3d97a2282c8ff17eab119d6325103fcdf9a8e1e
lmorais <lmorais.contato@gmail.com>
2023-01-26 10:44:30 -0300

    chore: add new library and tests scripts

--- jest.config.js
+++ jest.config.js
@@ -1,15 +1,18 @@
 
-const { compilerOptions } = require('./tsconfig.json');
 
 module.exports = {
   moduleFileExtensions: ['js', 'json', 'ts'],
   rootDir: 'src',
   testRegex: '.*\\.spec\\.ts$',
-  transform: {
-    '.+\\.(t|j)s$': 'ts-jest',
-  },
   maxWorkers: 1,
   collectCoverageFrom: ['<rootDir>/modules/**/services/*.useCase.(t|j)s', '<rootDir>/modules/**/utils/*.(t|j)s'],
   coverageDirectory: '../coverage',
   testEnvironment: 'node',
+  transform: {
+    '.+\\.ts$': 'ts-jest',
+  },
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/$1',
+    '^@/tests(.*)$': '<rootDir>/tests/$1',
+  },
 };
