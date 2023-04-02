47670be6ca29ca8e6f416289e2e9680f93fbd230
lmorais <lmorais.contato@gmail.com>
2023-04-01 21:47:14 -0300

    fix: configure dotenv in auth constant files

--- src/config/constants/auth.constants.ts
+++ src/config/constants/auth.constants.ts
@@ -1,4 +1,11 @@
+import * as dotenv from 'dotenv';
+
+dotenv.config();
+
 export const jwt = {
   secret: process.env.JWT_SECRET,
   expiresIn: process.env.JWT_EXPIRES_IN,
+  secretRefreshToken: process.env.REFRESH_TOKEN_SECRET,
+  expireInRefreshToken: process.env.REFRESH_TOKEN_EXPIRES_IN,
+  expireInRefreshTokenDays: process.env.REFRESH_TOKEN_EXPIRES_IN_DAYS,
 };
