1cda36134c3319bc6af89e237c0d2e16f9d05fa5
lmorais <lmorais.contato@gmail.com>
2023-04-03 16:17:13 -0300

    chore: add resources in user module

--- src/modules/users/users.module.ts
+++ src/modules/users/users.module.ts
@@ -1,11 +1,15 @@
 import {
   DATE_PROVIDER,
+  ETHEREAL_MAIL_PROVIDER,
   HASH_PROVIDER,
+  MAIL_TEMPLATE_PROVIDER,
 } from '@/config/constants/providers.constants';
 import {
   USERS_REPOSITORY,
   USERS_TOKENS_REPOSITORY,
 } from '@/config/constants/repositories.constants';
+import EtherealMailProvider from '@/shared/providers/MailerProvider/implementations/ethereal.provider';
+import { HandlebarMailTemplateProvider } from '@/shared/providers/MailTemplateProvider/implementations/handlebarsMailTemplate.provider';
 import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { TypeOrmModule } from '@nestjs/typeorm';
@@ -14,6 +18,8 @@ import { jwt } from '../../config/constants/auth.constants';
 import { AuthenticatedUserController } from './infra/http/controllers/AuthenticatedUser.controller';
 import { CreateUsersController } from './infra/http/controllers/CreateUser.controller';
 import { RefreshTokenController } from './infra/http/controllers/RefreshToken.controller';
+import { ResetPasswordUserController } from './infra/http/controllers/ResetPasswordUser.controller';
+import { SendForgotPasswordMailController } from './infra/http/controllers/SendForgotPasswordMail.controller';
 import { ShowProfileUserController } from './infra/http/controllers/ShowProfileUser.controller';
 import { UpdateProfileUserController } from './infra/http/controllers/UpdateProfileUser.controller';
 import { User } from './infra/typeorm/entities/User.entity';
@@ -28,6 +34,8 @@ import { BCryptHashProvider } from './providers/hashProvider/implementations/BCr
 import { AuthenticateUserService } from './services/AuthenticateUser.service';
 import { CreateUserService } from './services/CreateUser.service';
 import { RefreshTokenService } from './services/RefreshToken.service';
+import { ResetPasswordUserService } from './services/resetPasswordUser.service';
+import { SendForgotPasswordMailService } from './services/SendForgotPasswordMail.service';
 import { ShowProfileUserService } from './services/ShowProfileUser.service';
 import { UpdateUserService } from './services/UpdateUser.service';
 
@@ -47,6 +55,8 @@ import { UpdateUserService } from './services/UpdateUser.service';
     UpdateProfileUserController,
     ShowProfileUserController,
     RefreshTokenController,
+    SendForgotPasswordMailController,
+    ResetPasswordUserController,
   ],
   providers: [
     {
@@ -65,12 +75,22 @@ import { UpdateUserService } from './services/UpdateUser.service';
       provide: DATE_PROVIDER,
       useClass: DayjsDateProvider,
     },
+    {
+      provide: ETHEREAL_MAIL_PROVIDER,
+      useClass: EtherealMailProvider,
+    },
+    {
+      provide: MAIL_TEMPLATE_PROVIDER,
+      useClass: HandlebarMailTemplateProvider,
+    },
     JwtStrategy,
     AuthenticateUserService,
     CreateUserService,
     UpdateUserService,
     ShowProfileUserService,
     RefreshTokenService,
+    SendForgotPasswordMailService,
+    ResetPasswordUserService,
   ],
 })
 export class UsersModule {}
