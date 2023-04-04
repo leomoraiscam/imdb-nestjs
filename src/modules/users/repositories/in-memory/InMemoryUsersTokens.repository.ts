97a595691b2995a7495ffc66f5bd8ef9f9bbd0b1
lmorais <lmorais.contato@gmail.com>
2023-04-03 22:31:08 -0300

    feat: add more methods inside in memory users tokens

--- src/modules/users/repositories/in-memory/InMemoryUsersTokens.repository.ts
+++ src/modules/users/repositories/in-memory/InMemoryUsersTokens.repository.ts
@@ -3,10 +3,27 @@ import { v4 as uuid } from 'uuid';
 
 import { CreateUsersTokensDTO } from '../../dtos/requests/CreateUsersTokens.dto';
 import { UserToken } from '../../infra/typeorm/entities/UserToken.entity';
+import { IUsersTokensRepository } from '../UsersTokensRepository.interface';
 
 @Injectable()
-export class InMemoryUsersTokensRepository {
+export class InMemoryUsersTokensRepository implements IUsersTokensRepository {
   private usersTokens: UserToken[] = [];
+
+  async findByUserId(userId: string): Promise<UserToken> {
+    const userToken = this.usersTokens.find(
+      (userToken) => userToken.userId === userId,
+    );
+
+    return userToken;
+  }
+
+  async findByRefreshToken(token: string): Promise<UserToken> {
+    const userToken = this.usersTokens.find(
+      (userToken) => userToken.refreshToken === token,
+    );
+
+    return userToken;
+  }
 
   async findByUserIdAndRefreshToken(
     userId: string,
@@ -32,5 +49,13 @@ export class InMemoryUsersTokensRepository {
     this.usersTokens.push(userToken);
 
     return userToken;
+  }
+
+  async delete(id: string): Promise<void> {
+    const fieldIndex = this.usersTokens.findIndex(
+      (userToken) => userToken.id === id,
+    );
+
+    this.usersTokens.splice(fieldIndex, 1);
   }
 }
